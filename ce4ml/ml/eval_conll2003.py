00bba6780d5db228040ef394669d921757a53b57
Lars <lars@lars-michaelis.de>
2022-12-20 14:18:47 +0100

    added gpu support for conll2003 eval

--- ce4ml/ml/eval_conll2003.py
+++ ce4ml/ml/eval_conll2003.py
@@ -1,6 +1,8 @@
 import torch
 from datasets import Dataset, load_dataset
 from tqdm.auto import tqdm
+from accelerate import Accelerator
+import numpy as np
 
 from ..datasets.createDataset import tokenize_and_align_labels
 from .metrics import calculate_metrics, postprocess
@@ -16,7 +18,19 @@ def eval_conll2003(model, data_collator, tokenizer, batch_size, id2label):
     def filterForLocation(x):
         return [ (tag - 4) if tag in [5,6] else 0 for tag in x ]
     df["ner_tags"] = df["ner_tags"].apply(filterForLocation)
-    #print(df)
+    
+    # def filter_only_one_loc_max(x):
+    #     unique, counts = np.unique(x, return_counts=True)
+    #     counts_dict = dict(zip(unique, counts))
+    #     if 1 in counts_dict and counts_dict[1] > 1:
+    #         return False
+    #     else:
+    #         return True
+
+    # df_only_one_loc_max = df["ner_tags"].apply(filter_only_one_loc_max)
+    # df = df[df_only_one_loc_max]
+
+    # print(df)
     ds = Dataset.from_pandas(df)
 
     tok_ds = ds.map(
@@ -37,6 +51,11 @@ def eval_conll2003(model, data_collator, tokenizer, batch_size, id2label):
         batch_size=batch_size,
     )
 
+    accelerator = Accelerator()
+    model, eval_dataloader = accelerator.prepare(
+        model, eval_dataloader
+    )
+
     model.eval()
     with torch.no_grad():
         true_predictions, true_labels = [], []
@@ -47,7 +66,14 @@ def eval_conll2003(model, data_collator, tokenizer, batch_size, id2label):
             predictions = outputs.logits.argmax(dim=-1)
             labels = batch["labels"]
 
-            true_predictions_batch, true_labels_batch = postprocess(predictions, labels, id2label)
+            # Necessary to pad predictions and labels for being gathered
+            predictions = accelerator.pad_across_processes(predictions, dim=1, pad_index=-100)
+            labels = accelerator.pad_across_processes(labels, dim=1, pad_index=-100)
+
+            predictions_gathered = accelerator.gather(predictions)
+            labels_gathered = accelerator.gather(labels)
+
+            true_predictions_batch, true_labels_batch = postprocess(predictions_gathered, labels_gathered, id2label)
             true_predictions.extend(true_predictions_batch)
             true_labels.extend(true_labels_batch)
             
